bef591eeb6035488e6aef23b73cdae015eb9281a
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2019-03-03 00:08:49 -0500

    made muzzle flash and bullet trail

--- First Platformer/Assets/2d-mega-pack/Scripts/2D Platformer/Video08/Weapon.cs
+++ First Platformer/Assets/2d-mega-pack/Scripts/2D Platformer/Video08/Weapon.cs
@@ -6,7 +6,13 @@ public class Weapon : MonoBehaviour {
 	public float fireRate = 0;
 	public float Damage = 10;
 	public LayerMask whatToHit;
-	
+
+    public Transform BulletTrailPrefab;
+    float timeToSpawnEffect = 0;
+    public float effectSpawnRate = 10;
+
+    public Transform MuzzleFlashPrefab;
+
 	float timeToFire = 0;
 	Transform firePoint;
 
@@ -37,10 +43,25 @@ public class Weapon : MonoBehaviour {
 		Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 		Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
 		RaycastHit2D hit = Physics2D.Raycast (firePointPosition, mousePosition-firePointPosition, 100, whatToHit);
+        if(Time.time >= timeToSpawnEffect)
+        {
+            Effect();
+            timeToSpawnEffect = Time.time + 1 /effectSpawnRate;
+        }
 		Debug.DrawLine (firePointPosition, (mousePosition-firePointPosition)*100, Color.cyan);
 		if (hit.collider != null) {
 			Debug.DrawLine (firePointPosition, hit.point, Color.red);
 			Debug.Log ("We hit " + hit.collider.name + " and did " + Damage + " damage.");
 		}
 	}
+
+    void Effect()
+    {
+        Instantiate (BulletTrailPrefab, firePoint.position, firePoint.rotation);
+        Transform clone = Instantiate (MuzzleFlashPrefab, firePoint.position, firePoint.rotation) as Transform;
+        clone.parent = firePoint;
+        float size = Random.Range (0.6f, 0.9f);
+        clone.localScale = new Vector3(size, size, size);
+        Destroy(clone.gameObject, 0.02f);
+    }
 }
